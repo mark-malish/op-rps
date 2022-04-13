f7f44d536fae80309c3acd500102373b76ff88da
Mark Malish <mark.malish@gmail.com>
2022-04-13 14:23:56 -0500

    computerPlay function added

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+function computerPlay() {
+// math.random generates a number between 0 & 1, multiplied by 3 since there are three possible selections, math.ceil rounds up to nearest whole number     
+    let random = Math.ceil(Math.random() * 3);
+    if(random === 1) {
+        console.log('rock');
+    } else if(random === 2) {
+        console.log('paper');
+    } else {
+        console.log('scissors');
+    }
+    
+}
 
