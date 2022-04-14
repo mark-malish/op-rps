22bcbbe70031fe703edf286cfe9d9fc2ad1ac576
Mark Malish <mark.malish@gmail.com>
2022-04-14 09:27:44 -0500

    updated rps with win conditions for both players

--- main.js
+++ main.js
@@ -2,12 +2,37 @@ function computerPlay() {
 // math.random generates a number between 0 & 1, multiplied by 3 since there are three possible selections, math.ceil rounds up to nearest whole number     
     let random = Math.ceil(Math.random() * 3);
     if(random === 1) {
-        console.log('rock');
+        return 'rock';
     } else if(random === 2) {
-        console.log('paper');
+        return 'paper';
     } else {
-        console.log('scissors');
+        return 'scissors';
     }
     
 }
 
+function playRound(playerSelection, computerSelection) {
+    //player win conditions
+    if(playerSelection === 'rock' && computerSelection === 'scissors') {
+        return `You Win! Rock crushes scissors.`;
+    } else if(playerSelection === 'paper' && computerSelection === 'rock') {
+        return `You Win! Paper covers Rock.`;
+    } else if(playerSelection === 'scissors' && computerSelection === 'paper') {
+        return `You Win! Scissors cut Paper.`;
+    //player lose conditions
+    } else if(computerSelection === 'rock' && playerSelection === 'scissors') {
+        return `You Lose! Rock crushes scissors.`;
+    } else if(computerSelection === 'paper' && playerSelection === 'rock') {
+        return `You Lose! Paper covers Rock.`;
+    } else if(computerSelection === 'scissors' && playerSelection === 'paper') {
+        return `You Lose! Scissors cut Paper.`;
+    //game results in a draw
+    } else if(computerSelection === playerSelection) {
+        return `Draw! Play again.`
+    }
+}
+
+let playerSelection = prompt('please select either: rock paper or scissors');
+playerSelection = playerSelection.toLowerCase();
+let computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
