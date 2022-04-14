b64591cc0db32c8e20f594751e20b21950ff1ead
Mark Malish <mark.malish@gmail.com>
2022-04-14 11:46:09 -0500

    added function that takes player input for 5 games. plays 5 games against computer. console logs results for each game as well as final score at the end of 5 rounds

--- main.js
+++ main.js
@@ -11,28 +11,53 @@ function computerPlay() {
     
 }
 
+let playerScore = 0;
+let computerScore = 0;
+let draw = 0;
+
 function playRound(playerSelection, computerSelection) {
     //player win conditions
     if(playerSelection === 'rock' && computerSelection === 'scissors') {
-        return `You Win! Rock crushes scissors.`;
+        playerScore++;
+        return `You Win! Rock crushes scissors. Player: ${playerScore} Computer: ${computerScore}`;
     } else if(playerSelection === 'paper' && computerSelection === 'rock') {
-        return `You Win! Paper covers Rock.`;
+        playerScore++;
+        return `You Win! Paper covers Rock. Player: ${playerScore} Computer: ${computerScore}`;
     } else if(playerSelection === 'scissors' && computerSelection === 'paper') {
-        return `You Win! Scissors cut Paper.`;
+        playerScore++;
+        return `You Win! Scissors cut Paper. Player: ${playerScore} Computer: ${computerScore}`;
     //player lose conditions
     } else if(computerSelection === 'rock' && playerSelection === 'scissors') {
-        return `You Lose! Rock crushes scissors.`;
+        computerScore++;
+        return `You Lose! Rock crushes scissors. Player: ${playerScore} Computer: ${computerScore}`;
     } else if(computerSelection === 'paper' && playerSelection === 'rock') {
-        return `You Lose! Paper covers Rock.`;
+        computerScore++;
+        return `You Lose! Paper covers Rock. Player: ${playerScore} Computer: ${computerScore}`;
     } else if(computerSelection === 'scissors' && playerSelection === 'paper') {
-        return `You Lose! Scissors cut Paper.`;
+        computerScore++;
+        return `You Lose! Scissors cut Paper. Player: ${playerScore} Computer: ${computerScore}`;
     //game results in a draw
     } else if(computerSelection === playerSelection) {
-        return `Draw! Play again.`
+        draw++;
+        return `Draw! Play again. Player: ${playerScore} Computer: ${computerScore}`
+    }
+}
+// plays 5 rounds and console logs the results of each round with score tally. after 5 rounds console logs win/lose/tie with scores for each contender
+
+function game() {
+    for(let i = 0; i < 5; i++) {
+    let playerSelection = prompt('choose your weapon: rock paper or scissors');
+    playerSelection = playerSelection.toLowerCase();
+    let computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+    }
+    if(playerScore > computerScore) {
+        console.log(`You Win. Player: ${playerScore} Computer: ${computerScore} Draws: ${draw}`);
+    } else if(computerScore > playerScore) {
+        console.log(`You Lose. Player: ${playerScore} Computer: ${computerScore} Draws: ${draw}`)
+    } else if(playerScore === computerScore) {
+        console.log(`Tie Game. Player: ${playerScore} Computer: ${computerScore} Draws: ${draw}`)
     }
 }
 
-let playerSelection = prompt('please select either: rock paper or scissors');
-playerSelection = playerSelection.toLowerCase();
-let computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+game()
